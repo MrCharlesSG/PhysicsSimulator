7422e24a67dbf9d143ebc08d0f24b595dcd0aa5d
Nerkitres <118349690+Nerkitres@users.noreply.github.com>
2023-02-09 10:50:10 +0100

    Update NewtonUniversalGravitation.java

--- src/simulator/model/NewtonUniversalGravitation.java
+++ src/simulator/model/NewtonUniversalGravitation.java
@@ -1,5 +1,30 @@
 package simulator.model;
 
-public class NewtonUniversalGravitation {
+import java.util.List;
+
+import simulator.misc.Vector2D;
+
+public class NewtonUniversalGravitation implements ForceLaws{
+	
+	protected Double g;
+	
+	NewtonUniversalGravitation(Double G) throws IllegalArgumentException{
+		if(G < 0) {
+			throw new IllegalArgumentException();
+		}
+		else {
+			this.g=G;
+		}
+	}
+	
+	@Override
+	public void apply(List<Body> bs) {
+		// TODO Auto-generated method stub
+		for(Body i : bs) {
+			for(Body j:bs) {
+				bs.
+			}
+		}
+	}
 	
 }
