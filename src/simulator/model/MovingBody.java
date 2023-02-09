634f11eace2918f49672d6a71af2cb5a3d78bc87
Nerkitres <118349690+Nerkitres@users.noreply.github.com>
2023-02-09 10:08:30 +0100

    Moving Body

--- src/simulator/model/MovingBody.java
+++ src/simulator/model/MovingBody.java
@@ -4,17 +4,20 @@ import simulator.misc.Vector2D;
 
 public class MovingBody extends Body{
 	
-	protected int a;
-	
+	protected Vector2D a;
 	
 	MovingBody(String id, String gid, Vector2D vel, Vector2D pos, Double mass) throws IllegalArgumentException {
 		super(id, gid, vel, pos, mass);
-		this.a = 0;
+		this.a= new Vector2D();
 	}
 
 	@Override
 	void advance(Double dt) {
-		
+		if(this.masa!=0) {
+			a = a.scale(1/this.masa);
+			this.posicion = this.posicion.plus(velocidad.scale(dt).plus(a.scale(dt*dt/2)));
+			this.velocidad=this.velocidad.plus(a.scale(dt));
+		}
 	}
 	
 }
