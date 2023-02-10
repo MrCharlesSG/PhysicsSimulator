b2ba1ffc313592dacf6f54ae88cbac721c4ac123
Nerkitres <118349690+Nerkitres@users.noreply.github.com>
2023-02-10 12:33:47 +0100

    He creado los  metodos de PhysicsSimulator nada mas
    No se lo que es un map asi que no he podido hacer na

--- src/simulator/model/PhysicsSimulator.java
+++ src/simulator/model/PhysicsSimulator.java
@@ -1,5 +1,41 @@
 package simulator.model;
 
-public class PhysicsSimulator {
+import java.util.Map;
 
+import org.json.JSONObject;
+
+public class PhysicsSimulator {
+	
+	private Double dt;
+	private ForceLaws fl;
+	private String Id;
+	private Map<String,BodiesGroup> map; //No se de que va esto
+	
+	PhysicsSimulator(ForceLaws fl, Double dt) throws IllegalArgumentException {
+		if(fl==null||dt==null) throw new IllegalArgumentException();
+		else {
+			this.dt = dt;
+			this.fl = fl;
+		}
+	}
+	
+	public void advance(Double dt) { 
+		
+	}
+	
+	public void addBody(Body b) throws IllegalArgumentException{
+		
+	}
+	
+	public void setForceLaws(String id, ForceLaws f) throws IllegalArgumentException {
+		
+	}
+	
+	public JSONObject getState() { // No se hacerlo porque no se lo que es un map aun
+		JSONObject i = new JSONObject();
+		i.put("Id", this.Id);
+		
+		return i;
+	}
+	
 }
